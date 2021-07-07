2abb6cdd133eb5058f5fb529207f72929eddc705
nalinbot <nalinnagar1@gmail.com>
2021-07-07 17:27:47 -0400

    Created predict.py :fire:
    For predicting on images

---
+++ predict.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+import os
+import time
+
+from matplotlib import pyplot as plt
+from IPython import display
+
+# load an image
+def load_image(filename, size=(256,256)):
+	# load image with the preferred size
+	pixels = load_img(filename, target_size=size)
+	# convert to numpy array
+	pixels = img_to_array(pixels)
+	# scale from [0,255] to [-1,1]
+	pixels = (pixels - 127.5) / 127.5
+	# reshape to 1 sample
+	pixels = expand_dims(pixels, 0)
+	return pixels
+ 
+# load source image
+src_image = load_image('satellite.jpg')
+print('Loaded', src_image.shape)
+# load model
+model = load_model('model_109600.h5')
+# generate image from source
+gen_image = model.predict(src_image)
+# scale from [-1,1] to [0,1]
+gen_image = (gen_image + 1) / 2.0
+# plot the image
+pyplot.imshow(gen_image[0])
+pyplot.axis('off')
+pyplot.show()
